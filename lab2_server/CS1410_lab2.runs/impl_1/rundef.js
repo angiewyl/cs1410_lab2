8d3625d07c6bb67e0e23f6ec8e10138d4c069eeb
Angelina Wong <angiewyl@Angelinas-MacBook-Air-372.local>
2025-10-10 04:48:50 -0400

    lab2b done

--- lab2_server/CS1410_lab2.runs/impl_1/rundef.js
+++ lab2_server/CS1410_lab2.runs/impl_1/rundef.js
@@ -27,7 +27,7 @@ eval( EAInclude(ISEJScriptLib) );
 
 
 // pre-commands:
-ISETouchFile( "init_design", "begin" );
+ISETouchFile( "write_bitstream", "begin" );
 ISEStep( "vivado",
          "-log alu_top.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source alu_top.tcl -notrace" );
 
